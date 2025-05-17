c2d5a19fc3c3f7076715d15e1653b7df0462bb9b
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 18:31:54 -0700

    Update ESLint configuration and package dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
+const imageHosts = [
+  "images.unsplash.com",
+  "tailwindcss.com",
+  "plus.unsplash.com",
+  "img.freepik.com",
+  "media.istockphoto.com",
+  "www.freepik.com",
+  "cdn.pixabay.com",
+  "example.com",
+  "www.canva.com",
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: imageHosts.map((hostname) => ({
+      protocol: "https",
+      hostname,
+      port: "",
+      pathname: "/**",
+    })),
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
